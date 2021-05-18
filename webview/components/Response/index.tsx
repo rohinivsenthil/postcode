2e7b1d8127150bdbc57a60437cbbb9058bf0fe16
Rohini Senthil <rohinivsenthil@gmail.com>
2021-05-19 01:39:46 +0530

    (feat) add response window header

--- webview/components/Response/index.tsx
+++ webview/components/Response/index.tsx
@@ -2,14 +2,29 @@ import * as React from "react";
 import * as propTypes from "prop-types";
 import Editor from "@monaco-editor/react";
 import "./styles.css";
+import { responseViews } from "../../constants/response-views";
 
 export const Response = (props) => {
   const { response } = props;
-  console.log("in app", response);
   return (
     <div className="response-window">
+      <div className="response-header">
+        <div className="response-view-options">
+          <div>
+            {responseViews.map((type) => (
+              <button className="button-response-view" key={type.value}>
+                {type.name}
+              </button>
+            ))}
+          </div>
+          <div>Dropdown</div>
+        </div>
+        <div className="response-status">
+          <div>Status: {`${response.status} ${response.statusText}`}</div>
+        </div>
+      </div>
       <Editor
-        height="100%"
+        height="60%"
         language="HTML"
         theme="vs-dark"
         value={response.data}
---
+++ webview/constants/response-views.ts
@@ -0,0 +1,14 @@
+export const responseViews = [
+  {
+    name: "Pretty",
+    value: "pretty",
+  },
+  {
+    name: "Raw",
+    value: "raw",
+  },
+  {
+    name: "Preview",
+    value: "preview",
+  },
+];
