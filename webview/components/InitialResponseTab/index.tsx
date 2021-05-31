8434fe503cb335f5c9598cff49e5ae71b0f7b986
Rohini Senthil <rohinivsenthil@gmail.com>
2021-05-31 14:58:07 +0530

    (improv) minor css changes

--- webview/components/InitialResponseTab/index.tsx
+++ webview/components/InitialResponseTab/index.tsx
@@ -5,7 +5,7 @@ import { ReactComponent as PackageIcon } from "../../icons/package.svg";
 export const InitialResponse = () => {
   return (
     <div className="initial-response-wrapper">
-      <div>Hit Send to get a response</div>
+      <div className="initial-text">Hit Send to get a response</div>
       <PackageIcon className="img-initial-response" />
     </div>
   );
