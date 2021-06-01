8000ebde5ef53dbc017995aae19a5f67b55fe736
Mukundan314 <mukundan314@gmail.com>
2021-06-01 19:43:13 +0530

    (feat) setup redux

--- webview/index.tsx
+++ webview/index.tsx
@@ -1,11 +1,15 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import store from "./store";
 import "./index.css";
 import App from "./App";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
