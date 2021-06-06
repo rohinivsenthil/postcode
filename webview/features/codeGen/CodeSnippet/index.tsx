ae28ce8aed0c0768b4d710bcc728b78bdb051f99
Rohini Senthil <rohinivsenthil@gmail.com>
2021-06-06 11:54:17 +0530

    (improv) change options section to dropdown

--- webview/features/codeGen/CodeSnippet/index.tsx
+++ webview/features/codeGen/CodeSnippet/index.tsx
@@ -6,17 +6,23 @@ import { codeGenOptions } from "../codeGenSlice";
 export const CodeSnippet = () => {
   const [code, setCode] = React.useState("");
 
-  const buttons = codeGenOptions.flatMap(({ language, variants }) =>
-    variants.map((variant) => (
-      // eslint-disable-next-line react/jsx-key
-      <button className="option-button">{`${language} - ${variant}`}</button>
-    ))
-  );
-
   return (
     <div className="code-gen-wrapper">
-      <div className="code-lang-wrapper">
-        <div className="code-lang">{buttons}</div>
+      <div className="code-gen-option">
+        <select
+          // onChange={(e) => dispatch(requestAuthTypeUpdated(e.target.value))}
+          className="select-code-option"
+          // value={requestAuthType}
+        >
+          {codeGenOptions.flatMap(({ language, variants }) =>
+            variants.map((variant) => (
+              // eslint-disable-next-line react/jsx-key
+              <option value={`${language} - ${variant}`}>
+                {`${language} - ${variant}`}
+              </option>
+            ))
+          )}
+        </select>
       </div>
       <div className="code-display">
         <Editor
