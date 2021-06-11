803abdd81effa265c28717063d5db4f68266f637
Rohini Senthil <rohinivsenthil@gmail.com>
2021-06-11 18:13:28 +0530

    (improv) add teable to headers

--- webview/features/response/ResponseHeaders/index.tsx
+++ webview/features/response/ResponseHeaders/index.tsx
@@ -1,6 +1,18 @@
 import * as React from "react";
+import { KeyValueTable } from "../../../shared/KeyValueTable";
 // import "./styles.css";
 
 export const ResponseHeaders = () => {
-  return <div>Headers</div>;
+  return (
+    <div>
+      <KeyValueTable
+        data={[{}]}
+        // onRowAdd={(value) => dispatch(requestQueryParamAdded(value))}
+        // onRowDelete={(idx) => dispatch(requestQueryParamDeleted(idx))}
+        // onRowUpdate={(idx, value) =>
+        //   dispatch(requestQueryParamUpdated({ idx, value }))
+        // }
+      />
+    </div>
+  );
 };
